ad0ca71d375e71c029aaff14674034404badb225
jaykishan1saharan <jaikishansaharan@gmail.com>
2026-05-29 12:09:07 +0530

    updated local connections to public

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,15 @@
 const mysql = require('mysql2');
 
-// Create a connection pool to the MySQL database
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'manish',
-    database: process.env.DB_NAME || 'smart_billing_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Export a promise-based pool for async/await usage
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool.promise();
